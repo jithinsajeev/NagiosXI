79767494d601374b1570da8aee684b214b4415a2
jithinsajeev <jithin.sajeev73@gmail.com>
2020-03-12 10:57:47 +0530

    Create nrpe_agent.sh

---
+++ nrpe_agent.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -lt 1  ] ; then 
+  echo "Server IP should be provided as argument"
+  exit 1
+fi
+
+cd /tmp 
+curl -O https://assets.nagios.com/downloads/nagiosxi/agents/linux-nrpe-agent.tar.gz
+tar xzf linux-nrpe-agent.tar.gz
+cd linux-nrpe-agent
+./fullinstall -n -i $1
