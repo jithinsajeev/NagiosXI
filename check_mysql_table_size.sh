a79cc12e87252c0854c755a6fd366beafe1db604
Rajesh Sivaraman @rajesh_says <ksraju007@gmail.com>
2020-03-17 18:26:38 +0100

    improved script

--- check_mysql_table_size.sh
+++ check_mysql_table_size.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
+#Checks database size in mysql
+mysql --execute="SELECT table_schema 'Database Name',  SUM(data_length + index_length) 'Size in Bytes', ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) 'Size in MiB' FROM information_schema.tables WHERE table_schema = 'jithin'" > /tmp/new1.txt
 
-mysql --execute="SELECT 
-    table_schema 'Database Name',
-    SUM(data_length + index_length) 'Size in Bytes',
-    ROUND(SUM(data_length + index_length) / 1024 / 1024, 2) 'Size in MiB'
-FROM information_schema.tables 
-WHERE table_schema = 'jithin'" > /tmp/new1.txt
+DBSIZE=`sed -n 2p /tmp/new1.txt | cut -f 2`
 
-sed -n 2p /tmp/new1.txt > /tmp/new.txt
-
-cat /tmp/new.txt | cut -f 2 > /tmp/new1.txt
-
-rm -rf /tmp/new.txt
-
-a=`cat /tmp/new1.txt`
-
-if [ $a -gt 50000 ]
-then
+if [[ "$DBSIZE" -gt "50000" ]] ; then
 	echo "CRITICAL"
 else
 	echo "OK"
 fi
+exit 0
