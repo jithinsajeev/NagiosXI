0c239b8b262e6a341ecb314b3c2ee5ec656dbb59
Rajesh Sivaraman @rajesh_says <ksraju007@gmail.com>
2020-03-17 18:36:42 +0100

    added error checking

--- nrpe_agent.sh
+++ nrpe_agent.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
+#Installs nagios xi nrpe agent automatically.
 
-if [ $# -lt 1  ] ; then 
+if [[ "$#" -lt "1" ]] ; then 
   echo "Server IP should be provided as argument"
   exit 1
 fi
 
+#check if wget exists, and attempt to install.
+if `which wget > /dev/null` ; then
+  echo "wget found. continue."
+else
+  #install wget for centos/redhat
+  if [[ `which yum > /dev/null` ]] ; then
+    yum -y install wget    
+  fi
+  #install wget for ubuntu/debian
+  if [[ `which apt-get > /dev/null` ]] ; then
+    apt-get -y install wget    
+  fi
+fi
+
+
 cd /tmp 
-curl -O https://assets.nagios.com/downloads/nagiosxi/agents/linux-nrpe-agent.tar.gz
-tar xzf linux-nrpe-agent.tar.gz
-cd linux-nrpe-agent
-./fullinstall -n -i $1
+curl -O https://assets.nagios.com/downloads/nagiosxi/agents/linux-nrpe-agent.tar.gz 
+
+if [[ "$?" -ne "0" ]] ; then
+  echo "Downloding client failed."
+  exit 1
+fi
+
+tar xzf linux-nrpe-agent.tar.gz && cd linux-nrpe-agent && ./fullinstall -n -i $1
