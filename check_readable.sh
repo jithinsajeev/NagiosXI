74be6e474b481411adfc100138116e7200c3ffbe
jithinsajeev <jithin.sajeev73@gmail.com>
2020-03-18 18:54:29 +0530

    Update check_readable.sh

--- check_readable.sh
+++ check_readable.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # checking whether file is readable or not. 
-file=/etc/httpd/logs/access_log
-if [[ -r "$file" ]];
+file1=/etc/httpd/logs/access_log
+if [[ -r "$file1" ]];
 then
-	echo "File is readable"
+	echo "/etc/httpd/logs/access_log is readable"
 else
-	echo "File is not readable"
+	echo "/etc/httpd/logs/access_log is not readable"
+fi
+
+# Create file2.txt in /tmp for test. 
+file2=/tmp/file2.txt
+if [[ "$file2" ]];
+then
+	echo "/tmp/file2.txt is readable"
+else
+	echo "/tmp/file2.txt is readable is not readable"
 fi
